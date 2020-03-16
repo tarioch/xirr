a7924987d6378dbe9647dabefce8c6ba42ed1fbd
Patrick Ruckstuhl <patrick@ch.tario.org>
2020-03-16 21:54:54 +0000

    Fix failure when trying to call xirr with only a single entry

--- src/tests/test_math.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from datetime import datetime
-from xirr.math import xirr
-
-
-@pytest.mark.parametrize("valuesPerDateString,expected", [({'2019-12-31': -80005.8, '2020-03-12': 65209.6}, -0.6453638827)])
-def test_xirr(valuesPerDateString, expected):
-    valuesPerDate = {datetime.fromisoformat(k).date: v for k, v in valuesPerDateString.items()}
-    assert xirr(valuesPerDate) == expected
